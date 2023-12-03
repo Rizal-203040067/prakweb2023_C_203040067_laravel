f5d6d55b242216a64ad30433fbb1f058ca4c5783
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-04 00:52:51 +0700

    menyelesaikan vidio 4

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU Blog | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
+@extends('layouts.main')
 
-    <!-- <script src="js/script.js"></script> -->
-</body>
-</html>
+@section('container')
+    
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <h2 href="">
+                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+            </h2>
+            <h5>By: {{ $post["author"] }}</h5>
+            <p>{{  $post["body"] }}</p>
+        </article>
+    @endforeach
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home",
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Rizal Baihaqi",
         "email" => "203040067@mail.unpas.ac.id",
         "image" => "rizal.jpg"
@@ -27,5 +30,54 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rizal Baihaqi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequuntur quis mollitia perspiciatis ipsam, expedita veniam id illo vero impedit quibusdam! Delectus, fuga. Esse ad eos incidunt doloremque quisquam porro nihil enim earum quas deserunt adipisci, cupiditate nulla fugiat, aspernatur, eaque quo sit provident saepe deleniti atque praesentium. Illum similique magni adipisci corrupti totam. Dolores blanditiis aliquam, provident similique nobis alias pariatur itaque dignissimos harum aliquid ipsum quisquam, dolorum consectetur? Id accusantium animi cupiditate voluptates, atque doloribus velit quos eius!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptates, molestiae omnis error facere, quasi esse illo a similique sint pariatur laboriosam nobis vitae hic quidem cumque! Quis iste fugiat ratione, ullam animi maiores, doloribus fugit obcaecati iusto saepe earum. Dicta ea tenetur eveniet itaque rerum exercitationem et minus voluptatum sapiente mollitia quidem dolorum autem ullam necessitatibus fugit iusto, illo eaque impedit velit nam ipsum aliquid! Officia facilis dolorum temporibus blanditiis dolores ullam sed fugit eligendi aliquam saepe cumque voluptatum rerum laborum harum veniam aspernatur itaque deserunt dolore, voluptate nesciunt repellat. Ut aliquam accusamus dolore, perspiciatis sunt neque rerum natus?"
+        ]
+    ];
+
+    return view ('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rizal Baihaqi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequuntur quis mollitia perspiciatis ipsam, expedita veniam id illo vero impedit quibusdam! Delectus, fuga. Esse ad eos incidunt doloremque quisquam porro nihil enim earum quas deserunt adipisci, cupiditate nulla fugiat, aspernatur, eaque quo sit provident saepe deleniti atque praesentium. Illum similique magni adipisci corrupti totam. Dolores blanditiis aliquam, provident similique nobis alias pariatur itaque dignissimos harum aliquid ipsum quisquam, dolorum consectetur? Id accusantium animi cupiditate voluptates, atque doloribus velit quos eius!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptates, molestiae omnis error facere, quasi esse illo a similique sint pariatur laboriosam nobis vitae hic quidem cumque! Quis iste fugiat ratione, ullam animi maiores, doloribus fugit obcaecati iusto saepe earum. Dicta ea tenetur eveniet itaque rerum exercitationem et minus voluptatum sapiente mollitia quidem dolorum autem ullam necessitatibus fugit iusto, illo eaque impedit velit nam ipsum aliquid! Officia facilis dolorum temporibus blanditiis dolores ullam sed fugit eligendi aliquam saepe cumque voluptatum rerum laborum harum veniam aspernatur itaque deserunt dolore, voluptate nesciunt repellat. Ut aliquam accusamus dolore, perspiciatis sunt neque rerum natus?"
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
