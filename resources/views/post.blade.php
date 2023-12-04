4539da1bd32839d578fb2b18d88c3fb06da0c0f5
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-04 23:14:13 +0700

    menyelesaikan vidio 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,10 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By. Rizal Baihaqi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         
-   {!! $post->body !!}
+    {!! $post->body !!}
 
     {{-- // untuk menampilkan gambar pada posts --}}
     {{-- <img src="{{ Route('image.displayImage', $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}" class="img-fluid"> --}}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,22 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
 
 // untuk menampilkan gambar pada posts
 // Route::get('/storage/post-images/{filename}', [GetImagesController::class, 'displayImage']) -> name('image.displayImage');
