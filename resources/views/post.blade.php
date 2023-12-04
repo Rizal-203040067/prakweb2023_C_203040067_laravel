350ad7a61dcd6803fae50a11b6394c0d7edab3bc
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-04 15:23:42 +0700

    menyelesaikan vidio 5

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,12 @@
         <h2>{{ $post["title"] }}</h2>
         <h5>{{ $post["author"] }}</h5>
         <p>{{ $post["body"] }}</p>
+
+
+        {{-- // untuk menampilkan gambar pada posts --}}
+        {{-- <img src="{{ Route('image.displayImage', $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}" class="img-fluid"> --}}
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts">Back to Posts</a>
 @endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+// use App\Http\Controllers\GetImagesController;
+
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,55 +34,8 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rizal Baihaqi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequuntur quis mollitia perspiciatis ipsam, expedita veniam id illo vero impedit quibusdam! Delectus, fuga. Esse ad eos incidunt doloremque quisquam porro nihil enim earum quas deserunt adipisci, cupiditate nulla fugiat, aspernatur, eaque quo sit provident saepe deleniti atque praesentium. Illum similique magni adipisci corrupti totam. Dolores blanditiis aliquam, provident similique nobis alias pariatur itaque dignissimos harum aliquid ipsum quisquam, dolorum consectetur? Id accusantium animi cupiditate voluptates, atque doloribus velit quos eius!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptates, molestiae omnis error facere, quasi esse illo a similique sint pariatur laboriosam nobis vitae hic quidem cumque! Quis iste fugiat ratione, ullam animi maiores, doloribus fugit obcaecati iusto saepe earum. Dicta ea tenetur eveniet itaque rerum exercitationem et minus voluptatum sapiente mollitia quidem dolorum autem ullam necessitatibus fugit iusto, illo eaque impedit velit nam ipsum aliquid! Officia facilis dolorum temporibus blanditiis dolores ullam sed fugit eligendi aliquam saepe cumque voluptatum rerum laborum harum veniam aspernatur itaque deserunt dolore, voluptate nesciunt repellat. Ut aliquam accusamus dolore, perspiciatis sunt neque rerum natus?"
-        ]
-    ];
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
 
-    return view ('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rizal Baihaqi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequuntur quis mollitia perspiciatis ipsam, expedita veniam id illo vero impedit quibusdam! Delectus, fuga. Esse ad eos incidunt doloremque quisquam porro nihil enim earum quas deserunt adipisci, cupiditate nulla fugiat, aspernatur, eaque quo sit provident saepe deleniti atque praesentium. Illum similique magni adipisci corrupti totam. Dolores blanditiis aliquam, provident similique nobis alias pariatur itaque dignissimos harum aliquid ipsum quisquam, dolorum consectetur? Id accusantium animi cupiditate voluptates, atque doloribus velit quos eius!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptates, molestiae omnis error facere, quasi esse illo a similique sint pariatur laboriosam nobis vitae hic quidem cumque! Quis iste fugiat ratione, ullam animi maiores, doloribus fugit obcaecati iusto saepe earum. Dicta ea tenetur eveniet itaque rerum exercitationem et minus voluptatum sapiente mollitia quidem dolorum autem ullam necessitatibus fugit iusto, illo eaque impedit velit nam ipsum aliquid! Officia facilis dolorum temporibus blanditiis dolores ullam sed fugit eligendi aliquam saepe cumque voluptatum rerum laborum harum veniam aspernatur itaque deserunt dolore, voluptate nesciunt repellat. Ut aliquam accusamus dolore, perspiciatis sunt neque rerum natus?"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+// untuk menampilkan gambar pada posts
+// Route::get('/storage/post-images/{filename}', [GetImagesController::class, 'displayImage']) -> name('image.displayImage');
