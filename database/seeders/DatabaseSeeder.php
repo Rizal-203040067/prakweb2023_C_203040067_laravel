4fd8caa9f466216019ac3020d27dd4ec800712b1
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-06 01:05:22 +0700

    menyelesaikan vidio 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,63 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        User::create([
+            'name'  => 'Rizal Baihaqi',
+            'email' => '203040067@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+        
+        User::create([
+            'name'  => 'Sandhika Galih',
+            'email' => 'sandhikagalih@gmail.com',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+        
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam, quia laborum exercitationem ut doloremque nulla magnam velit natus vitae consequuntur esse? Necessitatibus debitis quo laudantium, enim esse, eius a quasi numquam perspiciatis eveniet laboriosam tenetur at! Beatae, fugiat, accusamus ad molestias nobis eum molestiae architecto harum perferendis totam odio dignissimos! Architecto quod ab quos magnam facilis, error nulla recusandae cum minima! Eum officiis quaerat fuga, sapiente consequatur possimus sed qui facere obcaecati a assumenda maiores velit architecto soluta reiciendis, earum laboriosam. Harum corporis sunt error?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam, quia laborum exercitationem ut doloremque nulla magnam velit natus vitae consequuntur esse? Necessitatibus debitis quo laudantium, enim esse, eius a quasi numquam perspiciatis eveniet laboriosam tenetur at! Beatae, fugiat, accusamus ad molestias nobis eum molestiae architecto harum perferendis totam odio dignissimos! Architecto quod ab quos magnam facilis, error nulla recusandae cum minima! Eum officiis quaerat fuga, sapiente consequatur possimus sed qui facere obcaecati a assumenda maiores velit architecto soluta reiciendis, earum laboriosam. Harum corporis sunt error?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam, quia laborum exercitationem ut doloremque nulla magnam velit natus vitae consequuntur esse? Necessitatibus debitis quo laudantium, enim esse, eius a quasi numquam perspiciatis eveniet laboriosam tenetur at! Beatae, fugiat, accusamus ad molestias nobis eum molestiae architecto harum perferendis totam odio dignissimos! Architecto quod ab quos magnam facilis, error nulla recusandae cum minima! Eum officiis quaerat fuga, sapiente consequatur possimus sed qui facere obcaecati a assumenda maiores velit architecto soluta reiciendis, earum laboriosam. Harum corporis sunt error?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, expedita et eaque, animi quam dignissimos labore facere fuga quaerat eveniet harum deleniti iure numquam, quia laborum exercitationem ut doloremque nulla magnam velit natus vitae consequuntur esse? Necessitatibus debitis quo laudantium, enim esse, eius a quasi numquam perspiciatis eveniet laboriosam tenetur at! Beatae, fugiat, accusamus ad molestias nobis eum molestiae architecto harum perferendis totam odio dignissimos! Architecto quod ab quos magnam facilis, error nulla recusandae cum minima! Eum officiis quaerat fuga, sapiente consequatur possimus sed qui facere obcaecati a assumenda maiores velit architecto soluta reiciendis, earum laboriosam. Harum corporis sunt error?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,14 +3,14 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. Rizal Baihaqi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         
     {!! $post->body !!}
 
     {{-- // untuk menampilkan gambar pada posts --}}
     {{-- <img src="{{ Route('image.displayImage', $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}" class="img-fluid"> --}}
 
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-3">Back to Posts</a>
 
 @endsection
 
