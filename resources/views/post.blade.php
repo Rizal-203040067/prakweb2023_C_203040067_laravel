91a1410c8ab68e979b138c0e3ca71742995d5fbf
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-11 22:01:41 +0700

    menyelesaikan vidio 12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        
-    {!! $post->body !!}
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
 
-    {{-- // untuk menampilkan gambar pada posts --}}
-    {{-- <img src="{{ Route('image.displayImage', $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}" class="img-fluid"> --}}
+                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+     
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
-    <a href="/posts" class="d-block mt-3">Back to Posts</a>
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+                {{-- // untuk menampilkan gambar pada posts --}}
+                {{-- <img src="{{ Route('image.displayImage', $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}" class="img-fluid"> --}}
+
+                <a href="/posts" class="d-block mt-3">Back to Posts</a>
+            </div>
+        </div>
+    </div>
 
 @endsection
 
